4cb3097eb8441e5a8f98b49b9039fa2fcab84536
maariagarrcia <mrrmn@myuax.com>
2021-11-30 12:19:12 +0100

    2.2

--- tarea2.py
+++ tarea2.py
@@ -1,4 +1,12 @@
 import random
+import pygame
+from pygame.locals import QUIT
+
+# Constantes para la inicialización de la superficie de dibujo
+VENTANA_HORI = 800  # Ancho de la ventana
+VENTANA_VERT = 600  # Alto de la ventana
+FPS = 60  # Fotogramas por segundo
+BLANCO = (255, 255, 255)  # Color del fondo de la ventana (RGB)
 
 # Definimos la clase pelota y dentro sus características
 class PelotaPong:
@@ -21,4 +29,3 @@ class PelotaPong:
     def mover(self):
         self.x += self.dir_x
         self.y += self.dir_y
-        
