21ea2a9e10ef2f968129ea1f74e47462986aa0d3
lauralardies <lrodrlar@myuax.com>
2021-11-30 17:20:01 +0100

    4.1.

---
+++ tarea4.py
@@ -0,0 +1,79 @@
+import random
+import pygame
+from pygame.locals import QUIT
+
+#Inicializamos pygame y definimos las constantes
+
+VENTANA_HORI = 800  
+VENTANA_VERT = 600  
+FPS = 60
+BLANCO = (255, 255, 255)  
+
+# Definimos la clase pelota y dentro sus características
+class PelotaPong:
+    def __init__(self, fichero_imagen):
+        # Características de la pelota 
+        # La imagen de la clase pelota
+        self.imagen = pygame.image.load(fichero_imagen).convert_alpha()
+
+        # Dimensiones de la Pelota
+        self.ancho, self.alto = self.imagen.get_size()
+
+        # Posición de la Pelota
+        self.x = VENTANA_HORI / 2 - self.ancho / 2
+        self.y = VENTANA_VERT / 2 - self.alto / 2
+
+        # Dirección de movimiento de la Pelota
+        self.dir_x = random.choice([-5, 5])
+        self.dir_y = random.choice([-5, 5])
+
+    def mover(self):
+        self.x += self.dir_x
+        self.y += self.dir_y
+
+# a continuación queremos crear una función para hacer que nuestra pelota rebote
+    def rebotar (self):
+        if self.x <= 0:
+            self.dir_x = - self.dir_x
+        if self.x + self.ancho >= VENTANA_HORI:
+            self.dir_x = - self.dir_x
+        if self.y <= 0:
+            self.dir_y = - self.dir_y
+        if self.y + self.alto >= VENTANA_VERT:
+            self.dir_y = - self.dir_y
+
+# añadimos una función para poder reiniciar la jugada
+    def reiniciar(self):
+        self.x = VENTANA_HORI / 2 - self.ancho / 2
+        self.y = VENTANA_VERT / 2 - self.alto / 2
+        self.dir_x = -self.dir_x
+        self.dir_y = random.choice([-5, 5])
+
+def main():
+    # Inicialización de Pygame
+    pygame.init()
+
+    # Inicialización de la superficie de dibujo (display surface)
+    ventana = pygame.display.set_mode((VENTANA_HORI, VENTANA_VERT))
+    pygame.display.set_caption("Pong 2")
+
+    pelota = PelotaPong("bola_roja.png")
+
+    jugando = True
+    while jugando:
+        pelota.mover()
+        pelota.rebotar()
+        ventana.fill(BLANCO)
+        ventana.blit(pelota.imagen, (pelota.x, pelota.y))
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                jugando = False
+
+        pygame.display.flip()
+        pygame.time.Clock().tick(FPS)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
