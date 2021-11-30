08d6c774ee8acc1018571ede724e1d6268fe67b7
maariagarrcia <mrrmn@myuax.com>
2021-11-30 12:15:56 +0100

    2.2

--- tarea2.py
+++ tarea2.py
@@ -1,1 +1,24 @@
 import random
+
+# Definimos la clase pelota y dentro sus características
+class PelotaPong:
+    def __init__(self, fichero_imagen):
+        # Características de la pelota 
+        # La imagen de la clase pelota
+        self.imagen = pygame.image.load(fichero_imagen).convert_alpha()
+
+        # Dimensiones de la Pelota
+        self.ancho, self.alto = self.imagen.get_size()
+
+        # Posición de la Pelota
+        self.x = VENTANA_HORI / 2 - self.ancho / 2
+        self.y = VENTANA_VERT / 2 - self.alto / 2
+
+        # Dirección de movimiento de la Pelota
+        self.dir_x = random.choice([-5, 5])
+        self.dir_y = random.choice([-5, 5])
+
+    def mover(self):
+        self.x += self.dir_x
+        self.y += self.dir_y
+        
