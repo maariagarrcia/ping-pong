8a3953ee64cff44d89ec690c3bef9134d4fa6696
maariagarrcia <mrrmn@myuax.com>
2021-11-30 18:21:34 +0100

    8.1

--- tarea8.py
+++ tarea8.py
@@ -2,48 +2,24 @@ import random
 import pygame
 from pygame.locals import QUIT
 
-
 #Inicializamos pygame y definimos las constantes
-
 VENTANA_HORI = 800  
 VENTANA_VERT = 600  
 FPS = 60
 BLANCO = (255, 255, 255)  
 
-def main():
-    pygame.init()
-    ventana = pygame.display.set_mode((VENTANA_HORI, VENTANA_VERT))
-    pygame.display.set_caption("Pong 1")
-
-#1.3 - Creamos el bucle principal del código
-    jugando = True
-    while jugando:
-        ventana.fill(BLANCO)
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                jugando = False
-        pygame.display.flip()
-        pygame.time.Clock().tick(FPS)
-    pygame.quit()
-
-if __name__ == "__main__":
-    main()
-
 # Definimos la clase pelota y dentro sus características
 class PelotaPong:
     def __init__(self, fichero_imagen):
-        # Características de la pelota 
-        # La imagen de la clase pelota
+        # ---- C A R A C T E R I S T I C A S
+        # La imagen
         self.imagen = pygame.image.load(fichero_imagen).convert_alpha()
-
-        # Dimensiones de la Pelota
+        # Dimensiones
         self.ancho, self.alto = self.imagen.get_size()
-
-        # Posición de la Pelota
+        # Posición 
         self.x = VENTANA_HORI / 2 - self.ancho / 2
         self.y = VENTANA_VERT / 2 - self.alto / 2
-
-        # Dirección de movimiento de la Pelota
+        # Dirección de movimiento 
         self.dir_x = random.choice([-5, 5])
         self.dir_y = random.choice([-5, 5])
 
@@ -51,26 +27,46 @@ class PelotaPong:
         self.x += self.dir_x
         self.y += self.dir_y
 
+# a continuación queremos crear una función para hacer que nuestra pelota rebote
+# nuestra función rebotar detecta que si la pelota sale por la izq o derecha, se inicia una nueva jugada
+    def rebotar(self):
+        if self.x <= -self.ancho:
+            self.reiniciar()
+        if self.x >= VENTANA_HORI:
+            self.reiniciar()
+        if self.y <= 0:
+            self.dir_y = -self.dir_y
+        if self.y + self.alto >= VENTANA_VERT:
+            self.dir_y = -self.dir_y
 
+# añadimos una función para poder reiniciar la jugada
+    def reiniciar(self):
+        self.x = VENTANA_HORI / 2 - self.ancho / 2
+        self.y = VENTANA_VERT / 2 - self.alto / 2
+        self.dir_x = -self.dir_x
+        self.dir_y = random.choice([-5, 5])
+
+# Añadimos las clase raqueta que es muy parecida al de la pelota y incluimos tambien sus caracteristicas       
 class RaquetaPong:
     def __init__(self):
         self.imagen = pygame.image.load("raqueta.png").convert_alpha()
-
-        # --- Atributos de la Clase ---
-
-        # Dimensiones de la Raqueta
+        #---- C A R A C T E R I S T I C A S
+        # Dimensiones
         self.ancho, self.alto = self.imagen.get_size()
-
-        # Posición de la Raqueta
+        # Posición
         self.x = 0
         self.y = VENTANA_VERT / 2 - self.alto / 2
-
-        # Dirección de movimiento de la Raqueta
+        # Dirección de movimiento
         self.dir_y = 0
-
+    
     def mover(self):
-        self.y += self.dir_y
-
+       self.y += self.dir_y
+       if self.y <= 0: 
+           self.y = 0
+       if self.y + self.alto >= VENTANA_VERT:
+           self.y = VENTANA_VERT - self.alto
+           
+    # Mover la raqueta con una estrategia definida  
     def mover_ia(self, pelota):
         if self.y > pelota.y:
             self.dir_y = -3
@@ -81,6 +77,18 @@ class RaquetaPong:
 
         self.y += self.dir_y
 
+#creamos una función que permita golpear la pelota
+    def golpear(self, pelota):
+        if (
+            pelota.x < self.x + self.ancho
+            and pelota.x > self.x
+            and pelota.y + pelota.alto > self.y
+            and pelota.y < self.y + self.alto
+        ):
+            pelota.dir_x = -pelota.dir_x
+            pelota.x = self.x + self.ancho
+
+
 def main():
     # Inicialización de Pygame
     pygame.init()
@@ -88,19 +96,21 @@ def main():
     # Inicialización de la superficie de dibujo (display surface)
     ventana = pygame.display.set_mode((VENTANA_HORI, VENTANA_VERT))
     pygame.display.set_caption("Pong 2")
-
     pelota = PelotaPong("bola_roja.png")
-
+    
+    # Creacion de las raquetas ya que  esta creada su clase, creando dos variables, modificando sus caracteristicas
     raqueta_1 = RaquetaPong()
     raqueta_1.x = 60
-
     raqueta_2 = RaquetaPong()
     raqueta_2.x = VENTANA_HORI - 60 - raqueta_2.ancho
 
     jugando = True
     while jugando:
         pelota.mover()
-
+        pelota.rebotar()
+        raqueta_1.mover()
+        raqueta_1.golpear(pelota)
+        # Dibujar raquetas
         ventana.fill(BLANCO)
         ventana.blit(pelota.imagen, (pelota.x, pelota.y))
         ventana.blit(raqueta_1.imagen, (raqueta_1.x, raqueta_1.y))
@@ -110,6 +120,20 @@ def main():
             if event.type == QUIT:
                 jugando = False
 
+        #Se detecta la pulsación de una tecla
+            if event.type == pygame.KEYDOWN:
+                if event.key== pygame.K_w:
+                    raqueta_1.dir_y = -5
+                if event.key == pygame.K_s:
+                    raqueta_1.dir_y = 5
+
+        #Se detecta que la tecla ha sido soltada
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    raqueta_1.dir_y = 0
+                if event.key == pygame.K_s:
+                    raqueta_1.dir_y = 0
+
         pygame.display.flip()
         pygame.time.Clock().tick(FPS)
 
@@ -117,6 +141,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
