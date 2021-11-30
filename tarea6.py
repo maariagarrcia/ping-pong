09eb5986760604251529808a13d8c25ae4d3ebb3
acasasaez <acasasae@myuax.com>
2021-11-30 19:49:40 +0100

    9.1

--- tarea6.py
+++ tarea6.py
@@ -20,6 +20,10 @@ class PelotaPong:
         # Dirección de movimiento 
         self.dir_x = random.choice([-5, 5])
         self.dir_y = random.choice([-5, 5])
+        #Puntación de la pelota
+        self.puntuacion = 0
+        self.puntuacion_ia = 0
+
     def mover(self):
         self.x += self.dir_x
         self.y += self.dir_y
@@ -28,8 +32,10 @@ class PelotaPong:
     def rebotar(self):
         if self.x <= -self.ancho:
             self.reiniciar()
+            self.puntuacion_ia += 1
         if self.x >= VENTANA_HORI:
             self.reiniciar()
+            self.puntuacion += 1
         if self.y <= 0:
             self.dir_y = -self.dir_y
         if self.y + self.alto >= VENTANA_VERT:
